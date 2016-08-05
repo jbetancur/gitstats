6fa0d52d7627c7f90e308a708159c843eec9464a
John Betancur <johnnyazee@gmail.com>
2016-08-05 14:26:32 -0400

    webpack config round 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,44 @@
+let webpack = require('webpack');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-	entry: {
-		app: 'src/app.ts',
-		vendor: 'src/vendor.ts'
-	},
-	output: {
-		path: './build',
-		filename: 'bundle-[hash:6].js'
-	},
-	loaders: [{
-		test: /\.ts$/,
-		loaders: 'ts'
-	},
-	{
-		test: /\.css$/,
-		loaders: 'style!css'
-	}]
+    entry: {
+        'vendor': './src/vendors.ts',
+        'app': './src/app.ts'
+    },
+    resolve: {
+        extensions: ['', '.js', '.ts']
+    },
+    output: {
+        path: './build',
+        filename: 'bundle-[hash:6].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loaders: ['ts', 'angular2-template-loader']
+            },
+            {
+                test: /\.css$/,
+                loaders: 'style!css'
+            },
+            {
+                test: /\.html$/,
+                loader: 'html'
+            }
+        ]
+    },
+    
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['vendor', 'app']
+        }),
+        
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+    
 };
